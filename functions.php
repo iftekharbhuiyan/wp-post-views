2004f132c60fbce9ec83145ae2a3f3efe2875b47
Iftekhar Bhuiyan <ibhuiyan@mail.com>
2024-04-26 13:15:38 -0400

    minor update

--- functions.php
+++ functions.php
@@ -1,33 +1,34 @@
 <?php
 /**
  * set post views meta key and value
- * @param int: post id
+ * @param int $post id
  */
 function set_post_views($post_id) {
-	if (is_numeric($post_id)) {
-		// set meta key
-		$meta_key = 'views';
-		// get meta value
-		$meta_value = get_post_meta($post_id, $meta_key, true);
-		if ($meta_value == '') {
-			// set meta value
-			$meta_value = 1;
-			// add meta key & value
-			add_post_meta($post_id, $meta_key, $meta_value);
-		} else {
-			// increase by 1
-			$meta_value++;
-			update_post_meta($post_id, $meta_key, $meta_value);
-		}
-	}
+    if (is_numeric($post_id)) {
+        $meta_key = 'post_views';
+        if (metadata_exists('post', $post_id, $meta_key)) {
+            // get value
+            $get_value = get_post_meta($post_id, $meta_key, true);
+            $meta_value = empty($get_value) ? 1 : $get_value++;
+            // update value
+            update_post_meta($post_id, $meta_key, $meta_value);
+        } else {
+            // set value
+            $meta_value = 1;
+            // add value
+            add_post_meta($post_id, $meta_key, $meta_value);
+        }
+    }
 }
-/* get post views count
- * @param int: post id
- * @return int: count
+
+/**
+ * get post views count
+ * @param int $post id
+ * @return int count
  */
 function get_post_views($post_id) {
-	if (is_numeric($post_id)) {
-		return get_post_meta($post_id, 'views', true);
-	}
+    if (is_numeric($post_id)) {
+        return get_post_meta($post_id, 'post_views', true);
+    }
 }
 ?>
